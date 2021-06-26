dd3d2a5ddbfd615dca0c49678ceabe484e53ad08
F66O <74874153+F66O@users.noreply.github.com>
2021-06-26 15:15:15 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,15 +1,3 @@
-Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-=== RESTART: C:/Users/dell/AppData/Local/Programs/Python/Python38/gggggggg.py ==
-Traceback (most recent call last):
-  File "C:/Users/dell/AppData/Local/Programs/Python/Python38/gggggggg.py", line 8, in <module>
-    eval(compile(aa(text),'<string>','exec'))
-  File "<string>", line 6, in <module>
-ModuleNotFoundError: No module named 'design'
->>> 
-=== RESTART: C:/Users/dell/AppData/Local/Programs/Python/Python38/gggggggg.py ==
-
 try:
     import sys
     from PyQt5 import QtGui
